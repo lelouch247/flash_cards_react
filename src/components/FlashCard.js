3f7a0389567bde2687368ae54b0ff44f4ad7ca96
Tate Jeppesen <jeppesenlelouch@gmail.com>
2017-06-19 22:57:41 -0600

    flash card mapping

--- src/components/FlashCard.js
+++ src/components/FlashCard.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
 class FlashCard extends React.Component {
-  state = { showAnswer : false }
+  render() {
+    let cards = this.props.cards.map( (card)  => {
+      return (<li>{card}</li>)
+    });
 
-render() {
-  return(
-    <div>
-
-    </div>
-  );
-}
-
+    return (
+      // possible solution? cards.title, return no error but does not render title
+      <ul>
+        {cards}
+      </ul>
+    )
+  }
 }
 
 export default FlashCard;
--- src/components/FlashCards.js
+++ src/components/FlashCards.js
@@ -2,35 +2,50 @@ import React from 'react'
 import FlashCard from './FlashCard'
 
 class FlashCards extends React.Component {
-  state = {cards: [
-                     {
-                      title: 'Card 1',
-                      front: 'What gem do you use for testing',
-                      back: 'rspec-rails'
-                    },
-                    {
-                      title: 'Card 2',
-                      front: 'What gem do you use to test code',
-                      back: 'pry'
-                    },
-                    {
-                      title: 'Card 3',
-                      front: 'What gem can you use for css',
-                      back: 'Matrialize-sass or bootstrap'
-                    },
-]};
+  constructor(props) {
+    super(props)
+      this.state = {cards: [
+        // I need Component FlashCards to scope here to get title and front
+        // possible solution? this.props.cards.title.map (defeats purpose of loop)
 
+        // Component FlashCard loops through the state cards and has var cards
+        // possible solution? maybe I can do card.title? (returns card 1 in browser)
 
-  displayFlashCards = () => {
-    return this.state.cards.map( card => {
-      return(<FlashCard card={card} />);
-    });
-  }
+        // return front and title, possible solution? card.title, card.front
+        // returns front: var but overwrites title
+        // I can write card.front somewhere else if it's still inside loop funct
+        // False I cannot write card.front cause it can only return once
+
+        { title: 'Card 1', front: 'What gem do you use for testing' }
+                        //  {
+                        //   title: 'Card 1',
+                        //   front: 'What gem do you use for testing',
+                        //   back: 'rspec-rails'
+                        // },
+                        // {
+                        //   title: 'Card 2',
+                        //   front: 'What gem do you use to test code',
+                        //   back: 'pry'
+                        // },
+                        // {
+                        //   title: 'Card 3',
+                        //   front: 'What gem can you use for css',
+                        //   back: 'Matrialize-sass or bootstrap'
+                        // },
+          ]};
+
+}
+
+  // displayFlashCards = () => {
+  //   return this.state.cards.map( card => {
+  //     return(<FlashCard card={card} />);
+  //   });
+  // }
 
   render() {
       return(
         <div>
-          { this.displayFlashCards() }
+            <FlashCard cards={this.state.cards} />
         </div>
       )
     }
